dc0b8007d33170759c999f2bd00263c77f5c6797
Ethan Matlin <ethanmatlin@gmail.com>
2020-05-06 13:33:14 -0400

    Comment out things that were not moved over

--- src/ReplicationFilesDSGE/includeall.jl
+++ src/ReplicationFilesDSGE/includeall.jl
@@ -7,29 +7,29 @@ using Plots.PlotMeasures, ColorTypes
 include("util.jl")
 
 # plot/
-include("plot/cond_obs_shocks_comparison_plot_forecast.jl")
-include("plot/plot_decomp_barplot_cond_obs_shocks.jl")
+#include("plot/cond_obs_shocks_comparison_plot_forecast.jl")
+#include("plot/plot_decomp_barplot_cond_obs_shocks.jl")
 include("plot/plot_brookings_irfs.jl")
-include("plot/plot_shockdecirfbar_cond_obs_shocks.jl")
-include("plot/plot_shockdecirfline_cond_obs_shocks.jl")
-include("plot/shock_groups.jl")
+#include("plot/plot_shockdecirfbar_cond_obs_shocks.jl")
+#include("plot/plot_shockdecirfline_cond_obs_shocks.jl")
+#include("plot/shock_groups.jl")
 include("plot/plot_4line.jl")
 
 # forecast/
-include("forecast/decomp_cond_obs_shocks.jl")
+#include("forecast/decomp_cond_obs_shocks.jl")
 include("forecast/cholesky_responses.jl")
 include("forecast/impulse_responses.jl")
 include("forecast/misc.jl")
 include("forecast/util.jl")
-include("forecast/dsgevar.jl")
-include("forecast/dsgevar_impulse_responses.jl")
-include("forecast/dsgevar_estimate.jl")
-include("forecast/var_impulse_responses.jl")
+#include("forecast/dsgevar.jl")
+#include("forecast/dsgevar_impulse_responses.jl")
+#include("forecast/dsgevar_estimate.jl")
+#include("forecast/var_impulse_responses.jl")
 
 # scenarios/
-include("scenarios/brookingsPC_scenarios.jl")
+#include("scenarios/brookingsPC_scenarios.jl")
 
 # write_tex/
-include("write_tex/write_cond_obs_shocks_comparison.jl")
-include("write_tex/write_brookings_irf.jl")
-include("write_tex/util.jl")
+#include("write_tex/write_cond_obs_shocks_comparison.jl")
+#include("write_tex/write_brookings_irf.jl")
+#include("write_tex/util.jl")
