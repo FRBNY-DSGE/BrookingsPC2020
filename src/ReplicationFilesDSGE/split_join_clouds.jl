c81e7f81543d7d6fe4998f7b36effc1a6bba58f5
Ethan Matlin <ethanmatlin@gmail.com>
2020-05-08 11:40:40 -0400

    Add cloud splitter/joiner script

---
+++ src/ReplicationFilesDSGE/split_join_clouds.jl
@@ -0,0 +1,26 @@
+using SMC
+split = true
+
+fp = "../../save_orig/output_data/m1002"
+
+filenames = ["$(fp)/ss10/estimate/raw/smc_cloud_npart=15000_period=r1_preZLB=true_reg2start=900331_vint=191118.jld2",
+             "$(fp)/ss10/estimate/raw/smc_cloud_npart=15000_period=r1_preZLB=true_reg2start=840331_vint=191118.jld2",
+             "$(fp)/ss10/estimate/raw/smc_cloud_npart=15000_period=r1_preZLB=false_reg2start=900331_vint=191118.jld2",
+             "$(fp)/ss21/estimate/raw/smc_cloud_period=full_incZLB_reg2start=900331_reg=2_vint=191118.jld2",
+             "$(fp)/ss21/estimate/raw/smc_cloud_period=full_preZLB_reg2start=840331_reg=2_vint=191118.jld2",
+             "$(fp)/ss21/estimate/raw/smc_cloud_period=full_preZLB_reg2start=900331_reg=2_vint=191118.jld2",
+             "$(fp)/ss22/estimate/raw/smc_cloud_friday=true_npart=20000_period=full_incZLB_reg2start=900331_reg=2_vint=191118.jld2",
+             "$(fp)/ss22/estimate/raw/smc_cloud_friday=true_npart=20000_period=full_preZLB_reg2start=900331_reg=2_vint=191118.jld2",
+             "$(fp)/ss22/estimate/raw/smc_cloud_friday=true_npart=20000_period=full_preZLB_reg2start=840331_reg=2_vint=191118.jld2",
+             "$(fp)/ss24/estimate/raw/smc_cloud_friday=true_npart=20000_period=full_incZLB_reg2start=900331_reg=2_vint=191118.jld2",
+             "$(fp)/ss24/estimate/raw/smc_cloud_friday=true_npart=20000_period=full_preZLB_reg2start=900331_reg=2_vint=191118.jld2",
+             "$(fp)/ss24/estimate/raw/smc_cloud_friday=true_npart=20000_period=full_preZLB_reg2start=840331_reg=2_vint=191118.jld2"]
+
+
+for filename in filenames
+    if split
+        split_cloud(filename, 4)
+    else
+        join_cloud(filename, 4)
+    end
+end
